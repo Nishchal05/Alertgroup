635ccc14f6a0836d7af1bfdac4432c6478b7de3a
nishchal05 <nishchalsundan04@gmail.com>
2025-02-22 23:57:48 +0530

    update joblisting.jsx

--- app/JoinPortal/Joblisting.jsx
+++ app/JoinPortal/Joblisting.jsx
@@ -17,7 +17,7 @@ const Joblisting = () => {
       {/* Job cards */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         {JobList.slice(list, list + 4).map((val, index) => (
-          <div key={index} className="border rounded-lg p-6 shadow-lg hover:shadow-xl transition-shadow duration-300 bg-slate-950 w-80">
+          <div key={index} className="border rounded-lg p-6 shadow-lg hover:shadow-xl transition-shadow duration-300 bg-slate-950 w-80 flex flex-col gap-4">
             <img src='/AlertGroup.png' alt='AlertGroup' width={100} />
             <h1 className="text-lg font-bold text-white mb-2">Position: {val.Position}</h1>
             <div className="text-md text-gray-400">Type: {val.Type}</div>
