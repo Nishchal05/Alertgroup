234bcfefd0b58781d4eb45e3b72899b6277d69f3
nishchal05 <nishchalsundan04@gmail.com>
2025-05-20 11:32:29 +0530

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://yourdomain.com', // replace with your actual domain
+    generateRobotsTxt: true, // also generate robots.txt
+  };
+  
